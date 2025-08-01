13c68d296bcb2f232446fc9c28819a49bf35ec7f
rikhard7707 <ricardoataide171@gmail.com>
2025-08-01 16:34:47 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,13 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker instalado');
-  self.skipWaiting();
-});
 
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker ativado');
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('verificador2-cache').then(function(cache) {
+      return cache.addAll(['./','./index.html','./manifest.json','./icon.png']);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response){
+    return response || fetch(event.request);
+  }));
 });
